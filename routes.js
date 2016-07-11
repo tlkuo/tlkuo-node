dd85156832771e7f4eaa3e788dca69dd2287736d
martin <tlkuo.frb@gmail.com>
2016-07-12 05:56:52 +0800

    Add more routes and logger for debug

--- routes.js
+++ routes.js
@@ -26,4 +26,48 @@ router.get("/:id", function (req, res) {
     });
 });
 
+
+// POST /questions/:id/answers
+// Route for creating an answer
+router.post("/:id/answers", function (req, res) {
+    res.json({
+        response: "You sent me a POST request to /answers",
+        questionId: req.params.id,
+        body: req.body
+    });
+});
+
+// PUT /questions/:qID/answers/:aID
+// Edit a specific answer
+router.put("/:qID/answers/:aID", function (req, res) {
+    res.json({
+        response: "You sent me a PUT request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// Delete a specific answer
+router.delete("/:qID/answers/:aID", function (req, res) {
+    res.json({
+        response: "You sent me a DELETE request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID
+    });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// Vote on a specific answer
+router.post("/:qID/answers/:aID/vote-:dir", function (req, res) {
+    res.json({
+        response: "You sent me a POST request to /vote-" + req.params.dir,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        vote: req.params.dir
+    });
+});
+
 module.exports = router;
