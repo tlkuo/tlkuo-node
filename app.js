35437ad391d1e142c0a236a4a54432512fa8c20d
martin <tlkuo.frb@gmail.com>
2016-07-12 05:14:03 +0800

    use middleware

--- app.js
+++ app.js
@@ -3,6 +3,22 @@
 var express = require("express");
 var app = express();
 
+app.use(function (req, res, next) {
+    req.myMessage = "Hello, middleware #2";
+    console.log("The leave on the trees are", req.query.color);
+    next();
+});
+
+app.use(function (req, res, next) {
+    console.log(req.myMessage);
+    next();
+});
+
+app.use("/different/:id", function (req, res, next) {
+    console.log("Third piece of middleware, ID:", req.params.id);
+    next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
