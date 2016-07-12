f51813566b7bad95e08234e0ccad57114ab8b061
martin <tlkuo.frb@gmail.com>
2016-07-12 08:15:46 +0800

    Add error handler

--- routes.js
+++ routes.js
@@ -61,13 +61,24 @@ router.delete("/:qID/answers/:aID", function (req, res) {
 // POST /questions/:qID/answers/:aID/vote-up
 // POST /questions/:qID/answers/:aID/vote-down
 // Vote on a specific answer
-router.post("/:qID/answers/:aID/vote-:dir", function (req, res) {
-    res.json({
-        response: "You sent me a POST request to /vote-" + req.params.dir,
-        questionId: req.params.qID,
-        answerId: req.params.aID,
-        vote: req.params.dir
-    });
-});
+router.post("/:qID/answers/:aID/vote-:dir",
+    function (req, res, next) {
+        if (req.params.dir.search(/^(up|down)$/) === -1) {
+            var err = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        } else {
+            next();
+        }
+    },
+    function (req, res) {
+        res.json({
+            response: "You sent me a POST request to /vote-" + req.params.dir,
+            questionId: req.params.qID,
+            answerId: req.params.aID,
+            vote: req.params.dir
+        });
+    }
+);
 
 module.exports = router;
