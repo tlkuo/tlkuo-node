f486cca11f26744dd6c05f86be4af0b788eeb2d8
martin <tlkuo.frb@gmail.com>
2016-07-13 02:55:24 +0800

    Fix typo

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods", "PUT, POST, DELETE')
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE')
         return res.status(200).json({})
     }
 
